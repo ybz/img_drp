def561343048dda5f48ee7fdc258f151f3a3b8c1
Yaniv Ben-Zaken <me@yaniv.bz>
2012-09-02 00:56:01 +0300

    move index's css to file and add a 'static' jinja filter

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
 app.config['DEBUG'] = bool(os.environ.get('DEBUG'))
+app.jinja_env.filters['static'] = lambda name: url_for('static', filename=name)
 
 @app.route('/')
 def index():
-    return render_template('index.tmpl', background_url=url_for('static', filename='circles.png'))
+    return render_template('index.tmpl')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))

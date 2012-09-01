74bd1f7781e868dd87190e0694353f104c903221
Yaniv Ben-Zaken <me@yaniv.bz>
2012-09-02 00:37:03 +0300

    add a background pattern for index

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.config['DEBUG'] = bool(os.environ.get('DEBUG'))
 
 @app.route('/')
 def index():
-    return render_template('index.tmpl')
+    return render_template('index.tmpl', background_url=url_for('static', filename='circles.png'))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))

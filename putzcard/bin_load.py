c56bc2f85056dda1d415bc97190ff62c3a9d6b21
Yaniv Ben-Zaken <me@yaniv.bz>
2012-09-14 04:07:40 +0300

    rename ENV variables

--- putzcard/bin_load.py
+++ putzcard/bin_load.py
@@ -5,7 +5,7 @@ import sys
 # OPEN_CV_PATH must be set up to be same as LD_LIBRARY_PATH in order to run, this differs in local systems and in heroku, so for right now will be explicitly needed to be set
 # TODO: set it up automatically
 
-OPEN_CV_PATH = os.environ['OPEN_CV_PATH']
+OPEN_CV_PATH = os.environ['OPENCV_PATH']
 sys.path.append(OPEN_CV_PATH)
 
 def open_cv_available():
@@ -25,12 +25,12 @@ def can_import_open_cv():
         return False
     
 def install_opencv_on_heroku():
-    assert ('OPEN_CV_PATH' in os.environ) and ('LD_LIBRARY_PATH' in os.environ) and (os.environ['OPEN_CV_PATH'] in os.environ['LD_LIBRARY_PATH']), 'missing OPEN_CV_PATH'
-    assert ('OPEN_CP_PACKAGE_URL' in os.environ), 'missing OPEN_CP_PACKAGE_URL'
+    assert ('OPENCV_PATH' in os.environ) and ('LD_LIBRARY_PATH' in os.environ) and (os.environ['OPENCV_PATH'] in os.environ['LD_LIBRARY_PATH']), 'missing OPENCV_PATH'
+    assert ('OPENCV_PACKAGE_URL' in os.environ), 'missing OPENCV_PACKAGE_URL'
     packages_folder = '/app/.heroku/vendor'
     package_name = 'open_cv_hrk.tar.gz'
     if not os.path.exists(packages_folder):
         os.mkdir(packages_folder)
-    os.system('curl -L %s -o %s' %(os.environ['OPEN_CP_PACKAGE_URL'], os.path.join(packages_folder, package_name)))
+    os.system('curl -L %s -o %s' %(os.environ['OPENCV_PACKAGE_URL'], os.path.join(packages_folder, package_name)))
     os.system('tar -xf %s -C %s' %(os.path.join(packages_folder, package_name), packages_folder) )
 
